0ef0cfd80364ff1216ae6fe7b55463d64de37b9c
TzviGreenfeld <tzvigreenfield@gmail.com>
2022-09-17 15:35:17 +0300

    time thing works. dosent find 2 1:00

--- tools/frame_detector.py
+++ tools/frame_detector.py
@@ -2,7 +2,6 @@ import easyocr
 import re
 import warnings
 from datetime import datetime, timedelta
-from turtle import RawTurtle, st
 from tqdm import tqdm
 
 warnings.filterwarnings("ignore", category=UserWarning)
@@ -14,14 +13,16 @@ class Frame:
         self.confidence = confidence
 
     def __gt__(self, other): # by confidence
-        return self.confidence > other.cnfidence
+        return self.confidence > other.confidence
     
-    def __sub__(self, other): # by time
-        t1 = datetime.fromtimestamp(self.stamp).strftime('%M:%S')
-        t2 = datetime.fromtimestamp(other.stamp).strftime('%M:%S')
-        delta = datetime.strptime(
-            max(t1, t2), '%M:%S') - datetime.strptime(min(t1, t2), '%M:%S')
-        return delta.total_seconds()
+    def min_time_diff(self, others): # by time
+        min_delta = timedelta(seconds=99999)
+        for frame in others:
+            t1 = datetime.strptime(self.stamp, "%M:%S")
+            t2 = datetime.strptime(frame.stamp, '%M:%S')
+            delta = max(t1, t2) - min(t1, t2)
+            min_delta = min(delta, min_delta)
+        return min_delta.seconds
 
 
 class FrameDetector:
@@ -37,40 +38,26 @@ class FrameDetector:
             result = self.reader.readtext(frame)
             for (rect, text, confidence) in result:
                 if re.match('1.00|1:00|2:00|2.00', text):
-                    self.found.append((stamp, rect, text, confidence))
+                    self.found.append(Frame(stamp, rect, text, confidence))
 
         self.detectionTime = f.format_dict["elapsed"]
         # sort by confidence
-        self.found.sort(key=lambda res: res[3], reverse=True)
+        self.found.sort(reverse=True)
 
     def get_best_results(self):
-        def time_diff(existing_frames, new_frame_time):
-            min_delta = timedelta(minutes=1000)
-            for frame in existing_frames:
-                t1 = datetime.fromtimestamp(frame[0]).strftime('%M:%S')
-                t2 = datetime.fromtimestamp(new_frame_time).strftime('%M:%S')
-                delta = datetime.strptime(
-                    max(t1, t2), '%M:%S') - datetime.strptime(min(t1, t2), '%M:%S')
-                min_delta = min(delta, min_delta)
-            return min_delta
-
         # TODO: test more episodes to set cinfidence filter
+        print("hi")
         best_frames = []
         one_cnt, two_cnt = 0, 0
-        last_stamp = 0
-
-        for stamp, rect, text, confidence in self.found:
-            if (text[0] == '1' and
-                one_cnt < 4 and
-                time_diff(best_frames, stamp) > timedelta(minutes=1)):
-                one_cnt += 1
-                best_frames.append((stamp, rect, text, confidence))
-
-            elif (text[0] == '2' and
-                  two_cnt == 0 and
-                  time_diff(best_frames, stamp) > timedelta(minutes=1)):
-                two_cnt += 1
-                best_frames.append((stamp, rect, text, confidence))
+        counts = [one_cnt, two_cnt]
+        for frame in self.found:
+            curr = 0 if frame.text[0] == '1' else 1
+            print(f"{counts[curr]=}, {one_cnt=}, {two_cnt=}")
+            if ((counts[curr] > 0 and frame.min_time_diff(best_frames) < 60) or
+                counts[curr] == 0):
+                best_frames.append(frame)
+                counts[curr] += 1
+                print(counts)
         return best_frames
 
     def __str__(self) -> str:
