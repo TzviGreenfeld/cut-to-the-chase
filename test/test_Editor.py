3b1660035ea65d5350dd49bbd48fccf54cfa437d
TzviGreenfeld <tzvigreenfield@gmail.com>
2022-10-01 21:20:31 +0300

    update tests

--- test/test_Editor.py
+++ test/test_Editor.py
@@ -1,10 +1,9 @@
 import os
 import unittest
 from math import ceil
+from tools import Editor, FrameDetector
 
-from tools.video_tools import Editor
-
-
+# TODO: this is testing older version of the object with different methods, update it
 class TestEditor(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
@@ -25,12 +24,6 @@ class TestEditor(unittest.TestCase):
         self.assertTrue(os.path.exists(name + ".mp4"))
         os.remove(name + ".mp4")
 
-    def test_grid(self):
-        i, j = 4, 4
-        grid = tester.grid(i, j, 3, 3)
-        est_h, est_w = grid[1], grid[3]  # TODO: isnt it backwards?
-
-        self.assertTrue(abs(est_h - tester.h) + abs(est_w - tester.w) < 10)
 
     def test_add_subclip(self):
         first = len(tester.clips)
@@ -62,7 +55,8 @@ class TestEditor(unittest.TestCase):
             return len([f for f in os.listdir(cwd) if f.endswith(".png")])
 
         before = count_png()
-        tester.get_frame_every_x_seconds(fps=1, grid=tester.grid(3, 3, 1, 2), blackAndWhite=True)
+        tester.get_frame_every_x_seconds(
+            fps=1, grid=tester.grid(3, 3, 1, 2), blackAndWhite=True)
         after = count_png()
         self.assertAlmostEqual(before + ceil(tester.duration.secs), after)
 
