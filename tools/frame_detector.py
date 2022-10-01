1605f59caa5d6dab78371f028d08a742485c6be9
TzviGreenfeld <tzvigreenfield@gmail.com>
2022-10-01 20:44:18 +0300

    merge files from develop

---
+++ tools/frame_detector.py
@@ -0,0 +1,77 @@
+import easyocr
+import re
+import warnings
+from datetime import datetime, timedelta
+from tqdm import tqdm
+
+warnings.filterwarnings("ignore", category=UserWarning)
+class Frame:
+    def __init__(self, stamp, rect, text, confidence):
+        self.stamp = stamp
+        self.rect = rect
+        self.text = text
+        self.confidence = confidence
+
+    def __gt__(self, other): # by confidence
+        return self.confidence > other.confidence
+    
+    def min_time_diff(self, others): # by time
+        # min_delta = timedelta(seconds=99999)
+        min_delta = 99999
+        for frame in others:
+            # t1 = datetime.strptime(self.stamp, "%M:%S")
+            # t2 = datetime.strptime(frame.stamp, '%M:%S')
+            # t1 = datetime.strptime(str(self.stamp), '%S')
+            # t2 = datetime.strptime(str(frame.stamp), '%S')
+            t1 = self.stamp
+            t2 = frame.stamp
+            delta = max(t1, t2) - min(t1, t2)
+            min_delta = min(delta, min_delta)
+        # return min_delta.seconds
+        return min_delta
+
+    def __repr__(self):
+        out = ""
+        out += "time: {}\t".format(self.stamp)
+        out += "text: {}\t".format(self.text)
+        out += "confidence: {}".format(self.confidence)
+        return out
+
+
+
+        
+
+class FrameDetector:
+    def __init__(self, frames):
+        self.reader = easyocr.Reader(['en'], gpu=True)
+        self.frames = frames
+        self.found = []
+        self.detection_time = -1
+
+    def detect(self):
+        f = tqdm(self.frames.items())
+        for stamp, frame in f:
+            result = self.reader.readtext(frame)
+            for (rect, text, confidence) in result:
+                if re.match('1.00|1:00|2:00|2.00', text):
+                    self.found.append(Frame(stamp, rect, text, confidence))
+
+        self.detectionTime = f.format_dict["elapsed"]
+        # sort by confidence
+        self.found.sort(reverse=True)
+
+    def get_best_results(self):
+        # TODO: test more episodes to set cinfidence filter
+        best_frames = []
+        one_cnt, two_cnt = 0, 0
+        counts = [one_cnt, two_cnt]
+        for frame in self.found:
+            curr = 0 if frame.text[0] == '1' else 1
+            if ((counts[curr] > 0 and frame.min_time_diff(best_frames) > 120) or
+                counts[curr] == 0):
+                best_frames.append(frame)
+                counts[curr] += 1
+        return best_frames
+
+    def __str__(self) -> str:
+        return f"Found {len(self.found)} time stamps in {self.detectionTime} seconds"
---
+++ tools/video_tools.py
@@ -0,0 +1,110 @@
+from re import A
+import moviepy.editor as mpy
+import numpy as np
+from pytube import YouTube
+from moviepy.editor import *
+
+class Duration:
+    def __init__(self, duration):
+        self.hours = (duration / 3600)
+        self.mins = (duration % 3600) / 60
+        self.secs = (duration % 60)
+
+
+class Editor:
+    def __init__(self, youtube_url=None, file_path=None):
+        self.original_file = None
+        self.youtube_url = youtube_url
+        self.file_path = file_path
+        if youtube_url is not None:
+            self.download(youtube_url)
+
+        if file_path is not None:
+            self.original_file = open(file_path, "r")
+
+        self.clips = []
+        self.VideoFileClip = mpy.VideoFileClip(
+            self.original_file.name)  # TODO: do i really need to open?
+        self.fps = self.VideoFileClip.fps
+        self.duration = Duration(self.VideoFileClip.duration)
+        self.w = self.VideoFileClip.w
+        self.h = self.VideoFileClip.h
+
+    def __del__(self):
+        if self.original_file is not None:
+            self.original_file.close()
+
+    def __str__(self):
+        out = ""
+        out += "from URL: " + \
+            (self.youtube_url if self.youtube_url is not None else "-----")
+        out += "\nfrom file: " + \
+            (self.file_path if self.file_path is not None else "-----")
+        out += "\nduration: " + str(self.duration)
+        out += "\nfps: " + str(self.fps)
+        out += "\nclips: " + str(len(self.clips))
+        out += "\nh, w: " + str(self.h) + "x" + str(self.w)
+        return out
+
+    def download(self, youtube_url):
+        """
+        download video from youtube
+        :param youtube_url: video link
+        :return:
+        """
+        # TODO: fix this function!
+        # test if valid url
+        video = YouTube(youtube_url)
+        # for s in video.streams.filter(file_extension="mp4"):
+        #     print(s)
+        # video.streams.filter(file_extension="mp4").order_by("fps").get_highest_resolution.download()
+        stream = video.streams.filter(
+            file_extension="mp4").get_highest_resolution()
+        # print(stream)
+        stream.download()
+        self.original_file = open(video.title + ".mp4", "r")
+ 
+
+    def add_subclip(self, start_time, duration):
+        """
+        append subClip to self.clips
+        :param start_time: first timestamp of subclip in seconds
+        :param duration: length of clip in (min, sec)
+        :return:
+        """
+        # test if time in range
+        end_time = start_time + duration
+        sub_clip = self.VideoFileClip.subclip(start_time, end_time)
+        self.clips.append(sub_clip)
+
+    def output_concatenated_sub_clips(self, output_file_name):
+        """
+        concatenate sub clips to one file
+        :param output_file_name: name, should end with some video file suffix
+        :return: true if succeeded, false otherwise
+        """
+        final_clip = mpy.concatenate_videoclips(self.clips)
+        try:
+            final_clip.write_videofile(output_file_name, codec="libx264")
+        except Exception as e:
+            print(e) 
+            return False
+
+        return True
+
+    def get_frame_every_x_seconds(self, x, crop=None, blackAndWhite=False):
+        """
+        get frame every x seconds
+        :param x: seconds
+        :param crop: tuple of (y1, y2, x1, x2)
+        :param blackAndWhite: if true, convert to black and white
+        :return: iterator of frames where frames[i][0] is a timestamp in seconds as numpy.float64 and frames[i][1] is a frame
+
+        """
+        clip = self.VideoFileClip
+        if crop:
+            clip = clip.crop(y1=crop[0], y2=crop[1], x1=crop[2], x2=crop[3])
+        if blackAndWhite:
+            clip = clip.fx(vfx.blackwhite)
+        # detector is using {time:frame} and here we are using [time, frame]
+        return clip.iter_frames(fps=(1 / x), with_times=True, dtype="uint8")
